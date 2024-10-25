c9e99474fe02f50a7d2c18101fcf73ecc9aa606a
Fernando Salom <fer@rudo.es>
2024-10-25 05:38:26 +0200

    Fix mapper domain to dto

--- driving/api_rest/v1/tickets/mapper.py
+++ driving/api_rest/v1/tickets/mapper.py
@@ -1,3 +1,4 @@
+from domain.date_range import DateRange
 from domain.product import Product
 from domain.ticket import Ticket
 from driving.api_rest.v1.tickets.models import AllTicketsResponse, TicketResponse, ProductResponse
@@ -23,4 +24,12 @@ class TicketDTOMapper:
 
     @staticmethod
     def products_to_dto(products: [Product]) -> [ProductResponse]:
-        return [ProductResponse(name=p.name, quantity=p.quantity, price=p.price) for p in products]
+        return [ProductResponse(name=product.name,
+                                quantity=product.quantity,
+                                price_per_unit=product.price_per_unit,
+                                price=product.price,
+                                weight=None) for product in products]
+
+    @staticmethod
+    def date_str_to_domain(start_date: str, end_date: str) -> DateRange:
+        return DateRange(start_date=start_date, end_date=end_date)
