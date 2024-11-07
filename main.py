6acfd8ebda4abd6c596e554dba89c67e11bf20f3
Fernando Salom <fer@rudo.es>
2024-11-07 11:16:43 +0100

    Add new log limitation and cors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import django
+from starlette.middleware.cors import CORSMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infrastructure.django.settings')
 django.setup()
@@ -9,5 +10,18 @@ from driving.api_rest.router import add_routers
 from driving.api_rest.containers import add_containers
 
 app = FastAPI()
+origins = [
+    "http://localhost:3000",  # Origen de desarrollo
+    "https://tickets.rudo.es"   # Puedes añadir otros orígenes de producción aquí
+]
+
+# Agrega el middleware de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,                # Permite estos orígenes
+    allow_credentials=True,                # Permitir cookies y autenticación
+    allow_methods=["*"],                   # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],                   # Permitir todos los headers
+)
 add_routers(app)
 add_containers(app)
