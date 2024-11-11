a80234d90be766cd395235fdf9c684b60e532a85
Fernando Salom <fer@rudo.es>
2024-11-11 08:21:26 +0100

    Update fail2ban

--- .docker/nginx/dev/entrypoint.sh
+++ .docker/nginx/dev/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+cp /config/fail2ban-logrotate.conf /etc/logrotate.d/fail2ban
 service fail2ban start
-
 while true; do
     logrotate /etc/logrotate.d/fail2ban
-    sleep 86400
+    sleep 86400  # Espera de 24 horas (86400 segundos)
 done &
 
 nginx -g 'daemon off;'
