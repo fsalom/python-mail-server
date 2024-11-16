e15e5f57e6034c1837e2a4cdd8818c6a17c6b4db
Fernando Salom <fer@rudo.es>
2024-11-16 11:21:33 +0100

    Add user manager and model

--- driven/db/user/models.py
+++ driven/db/user/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
+from .managers import UserManager
 
 
 class UserDBO(AbstractUser):
-    pass
+    objects = UserManager()
+
