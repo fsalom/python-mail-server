c31072eb1bc9f5b28c5cf2fbc5736f6afcc3239f
Fernando Salom <fer@rudo.es>
2024-11-20 11:32:52 +0100

    Add db user

--- driven/db/user/adapter.py
+++ driven/db/user/adapter.py
@@ -1,7 +1,23 @@
 from application.ports.driven.database.user.db_repository import UserDBRepositoryPort
 from domain.user import User
+from driven.db.user.mapper import UserDBMapper
+from driven.db.user.models import UserDBO
 
 
 class UserDBRepositoryAdapter(UserDBRepositoryPort):
-    def get(self, user: str) -> User:
-        pass
+    def __init__(self, mapper: UserDBMapper):
+        self.mapper = mapper
+
+    def get(self, email: str) -> User:
+        try:
+            user = UserDBO.objects.get(email=email)
+            return self.mapper.from_dbo_to_domain(user)
+        except Exception as e:
+            return None
+
+    def get_or_create_user_by_email(self, email: str) -> User | None:
+        try:
+            user, _ = UserDBO.objects.get_or_create(email=email)
+            return self.mapper.from_dbo_to_domain(user)
+        except Exception as e:
+            return None
