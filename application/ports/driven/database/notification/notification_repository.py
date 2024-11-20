e68746b596deb7e09ea377e57ce587f1055bb40e
Fernando Salom <fer@rudo.es>
2024-11-20 11:35:31 +0100

    Add ports for notification database adapter

---
+++ application/ports/driven/database/notification/notification_repository.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from domain.device import Device
+from domain.notification import Notification
+from domain.user import User
+
+
+class NotificationDBRepositoryPort(ABC):
+    @abstractmethod
+    def create_device(self, device: Device, user: User):
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_devices(self, user: User) -> List[Device]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def create_notification(self, notification: Notification) -> Notification:
+        raise NotImplementedError
+
+    @abstractmethod
+    def add_notification_to_user(self, notification: Notification, user: User):
+        raise NotImplementedError
