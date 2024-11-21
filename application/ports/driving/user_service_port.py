2ceddf6bef89faa6c2321849ab3b90619b27d0e2
Fernando Salom <fer@rudo.es>
2024-11-21 09:24:48 +0100

    Add user service

---
+++ application/ports/driving/user_service_port.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+from domain.user import User
+
+
+class UserServicePort(ABC):
+    @abstractmethod
+    async def get(self, email: str) -> User:
+        pass
+
+    @abstractmethod
+    async def update_fcm_token(self, user: User, token: str):
+        pass
---
+++ application/services/user_services.py
@@ -0,0 +1,14 @@
+from application.ports.driven.database.user.db_repository import UserDBRepositoryPort
+from application.ports.driving.user_service_port import UserServicePort
+from domain.user import User
+
+
+class UserServices(UserServicePort):
+    def __init__(self, db_repository: UserDBRepositoryPort):
+        self.db_repository = db_repository
+
+    async def get(self, email: str) -> User:
+        return self.db_repository.get(email)
+
+    async def update_fcm_token(self, user: User, token: str):
+        return await self.db_repository.update_fcm_token(user, token)
