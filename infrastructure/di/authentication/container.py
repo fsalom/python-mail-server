50d4adbc04acf80b06270042899c401d20d8e0fe
Fernando Salom <fer@rudo.es>
2024-11-21 09:26:05 +0100

    Add user db repository to user adapter

--- infrastructure/di/authentication/container.py
+++ infrastructure/di/authentication/container.py
@@ -4,6 +4,8 @@ from fastapi.security import OAuth2PasswordBearer
 from application.services.authentication_services import AuthServices
 from driven.apple.adapter import AppleRepositoryAdapter
 from driven.db.authentication.adapter import AuthenticationDBRepositoryAdapter
+from driven.db.user.adapter import UserDBRepositoryAdapter
+from driven.db.user.mapper import UserDBMapper
 from driven.google.adapter import GoogleRepositoryAdapter
 
 
@@ -12,6 +14,10 @@ class AuthContainer(containers.DeclarativeContainer):
     wiring_config = containers.WiringConfiguration(modules=["driving.api_rest.security",
                                                             "driving.api_rest.v1.authentication.adapter"])
     db_repository = providers.Factory(AuthenticationDBRepositoryAdapter)
+    user_db_repository = providers.Factory(
+        UserDBRepositoryAdapter,
+        mapper=UserDBMapper()
+    )
     google_repository = providers.Factory(GoogleRepositoryAdapter)
     apple_repository = providers.Factory(AppleRepositoryAdapter)
     oauth2_scheme = providers.Singleton(OAuth2PasswordBearer, tokenUrl="token")
@@ -21,4 +27,5 @@ class AuthContainer(containers.DeclarativeContainer):
         db_repository=db_repository,
         google_repository=google_repository,
         apple_repository=apple_repository,
+        user_db_repository=user_db_repository
     )
