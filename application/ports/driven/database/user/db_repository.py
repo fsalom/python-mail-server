49712cc8aa1bfa2dbc8e888df454b509f14e567f
Fernando Salom <fer@rudo.es>
2024-11-21 09:27:02 +0100

    Update db_repository.py

--- application/ports/driven/database/user/db_repository.py
+++ application/ports/driven/database/user/db_repository.py
@@ -11,3 +11,7 @@ class UserDBRepositoryPort(ABC):
     @abstractmethod
     def get_or_create_user_by_email(self, email: str) -> User | None:
         pass
+
+    @abstractmethod
+    def update_fcm_token(self, user: User, token: str):
+        pass
