994cd2d6fc7a9873d1ae3c2a2a3a45032f387b8a
Fernando Salom <fer@rudo.es>
2024-11-21 09:27:13 +0100

    Add new user container

--- driving/api_rest/containers.py
+++ driving/api_rest/containers.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI
 
 from infrastructure.di.authentication.container import AuthContainer
 from infrastructure.di.tickets.container import TicketContainer
+from infrastructure.di.user.container import UserContainer
 
 
 def add_containers(app: FastAPI):
     authentication_container = AuthContainer()
     tickets_container = TicketContainer()
+    user_container = UserContainer()
     app.authentication_container = authentication_container
     app.tickets_container = tickets_container
+    app.user_container = user_container
